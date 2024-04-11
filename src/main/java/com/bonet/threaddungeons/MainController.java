eb9688eb618f92cd150c84454ca379a96a89505a
Bonet27 <97919237+Bonet27@users.noreply.github.com>
2024-04-12 01:48:49 +0200

    Mejorado visual escena principal

--- src/main/java/com/bonet/threaddungeons/MainController.java
+++ src/main/java/com/bonet/threaddungeons/MainController.java
@@ -1,11 +1,11 @@
 package com.bonet.threaddungeons;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class MainController {
     @FXML
@@ -34,61 +34,104 @@ public class MainController {
     private TitledPane botin4pane;
     @FXML
     private TitledPane botin5pane;
+    @FXML
+    private Accordion accordion1;
+    @FXML
+    private Accordion accordion2;
+    @FXML
+    private Label enemy1HpLabel;
+    @FXML
+    private Label enemy2HpLabel;
+    @FXML
+    private Label enemy3HpLabel;
+    @FXML
+    private Label enemy4HpLabel;
+    @FXML
+    private Label enemy5HpLabel;
 
     @FXML
     private void initialize() {
-
-        nivel1pane.setExpanded(true);
-        botin1pane.setExpanded(true);
+        accordion1.setExpandedPane(nivel1pane);
+        accordion2.setExpandedPane(botin1pane);
 
         // Agregar listeners para el evento de expansión/cierre de los TitledPane
         nivel1pane.expandedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Si nivel1pane se expande, expandir también botin1pane
                 botin1pane.setExpanded(true);
+                FadeTransition fadeInTransition = new FadeTransition(Duration.millis(500), enemy1HpLabel);
+                fadeInTransition.setFromValue(0.0);
+                fadeInTransition.setToValue(1.0);
+                fadeInTransition.play();
             } else {
-                // Si nivel1pane se cierra, cerrar también botin1pane
                 botin1pane.setExpanded(false);
+                FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(500), enemy1HpLabel);
+                fadeOutTransition.setFromValue(1.0);
+                fadeOutTransition.setToValue(0.0);
+                fadeOutTransition.play();
             }
         });
 
         nivel2pane.expandedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Si nivel1pane se expande, expandir también botin1pane
                 botin2pane.setExpanded(true);
+                FadeTransition fadeInTransition = new FadeTransition(Duration.millis(500), enemy2HpLabel);
+                fadeInTransition.setFromValue(0.0);
+                fadeInTransition.setToValue(1.0);
+                fadeInTransition.play();
             } else {
-                // Si nivel1pane se cierra, cerrar también botin1pane
                 botin2pane.setExpanded(false);
+                FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(500), enemy2HpLabel);
+                fadeOutTransition.setFromValue(1.0);
+                fadeOutTransition.setToValue(0.0);
+                fadeOutTransition.play();
             }
         });
 
         nivel3pane.expandedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Si nivel1pane se expande, expandir también botin1pane
                 botin3pane.setExpanded(true);
+                FadeTransition fadeInTransition = new FadeTransition(Duration.millis(500), enemy3HpLabel);
+                fadeInTransition.setFromValue(0.0);
+                fadeInTransition.setToValue(1.0);
+                fadeInTransition.play();
             } else {
-                // Si nivel1pane se cierra, cerrar también botin1pane
                 botin3pane.setExpanded(false);
+                FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(500), enemy3HpLabel);
+                fadeOutTransition.setFromValue(1.0);
+                fadeOutTransition.setToValue(0.0);
+                fadeOutTransition.play();
             }
         });
 
         nivel4pane.expandedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Si nivel1pane se expande, expandir también botin1pane
                 botin4pane.setExpanded(true);
+                FadeTransition fadeInTransition = new FadeTransition(Duration.millis(500), enemy4HpLabel);
+                fadeInTransition.setFromValue(0.0);
+                fadeInTransition.setToValue(1.0);
+                fadeInTransition.play();
             } else {
-                // Si nivel1pane se cierra, cerrar también botin1pane
                 botin4pane.setExpanded(false);
+                FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(500), enemy4HpLabel);
+                fadeOutTransition.setFromValue(1.0);
+                fadeOutTransition.setToValue(0.0);
+                fadeOutTransition.play();
             }
         });
 
         nivel5pane.expandedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                // Si nivel1pane se expande, expandir también botin1pane
                 botin5pane.setExpanded(true);
+                FadeTransition fadeInTransition = new FadeTransition(Duration.millis(500), enemy5HpLabel);
+                fadeInTransition.setFromValue(0.0);
+                fadeInTransition.setToValue(1.0);
+                fadeInTransition.play();
             } else {
-                // Si nivel1pane se cierra, cerrar también botin1pane
                 botin5pane.setExpanded(false);
+                FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(500), enemy5HpLabel);
+                fadeOutTransition.setFromValue(1.0);
+                fadeOutTransition.setToValue(0.0);
+                fadeOutTransition.play();
             }
         });
 
